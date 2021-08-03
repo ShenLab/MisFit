43a247ea6993d62a2efc64eee71753466481f319
alantian2018 <74695538+alantian2018@users.noreply.github.com>
2021-08-02 18:14:07 -0700

    Revert "Add files via upload"
    This reverts commit 06640129ca2a7d16f3df72f42b36244496d87c5a.

--- embed.py
+++
@@ -1,6 +0,0 @@
-
-import os
-#l=[0,20000,40000,60000,80000,100000]
-#for i in l:
-os.system('python extract.py esm1b_t33_650M_UR50S {} /data/alant/ESM/train_embeddings/orig \
- --include mean per_tok'.format(os.path.join('/data/alant/ESM/fasta_files','{}.fasta'.format('0_orig'))))
