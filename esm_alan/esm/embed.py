a618d7cd2b1de5665091116e43bd39ea54049987
Yufeng Shen <joshuashen@gmail.com>
2023-12-06 22:13:24 -0500

    clean up

--- esm_alan/esm/embed.py
+++
@@ -1,6 +0,0 @@
-
-import os
-
-
-os.system('python extract.py esm1b_t33_650M_UR50S {} /data/alant/ESM/train_embeddings/orig \
- --include mean per_tok'.format(os.path.join('/data/alant/ESM/fasta_files','{}.fasta'.format('0_orig'))))
